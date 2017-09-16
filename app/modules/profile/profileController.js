60aed31ae5dbf8e7db8c0fe07fbdeabb6de51716
thomasdelagoutine <thomasdelagoutine@gmail.com>
2017-09-16 19:11:39 +0200

    Previsualisatin des images de profil

--- app/modules/profile/profileController.js
+++ app/modules/profile/profileController.js
@@ -3,5 +3,31 @@
  */
 app.controller('profileController', ['$scope', '$location',
     function ($scope, $location) {
-        
+
+        $scope.profilePhoto = null;
+        $scope.profilePhotoToDisplay = null;
+
+        $scope.isImage = function(ext) {
+            if(ext) {
+                return ext == "jpg" || ext == "jpeg"|| ext == "gif" || ext=="png"
+            }
+        };
+
+        function readURL(input) {
+
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#blah').attr('src', e.target.result);
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        $("#imgInp").change(function(){
+            readURL(this);
+        });
+
     }]);
